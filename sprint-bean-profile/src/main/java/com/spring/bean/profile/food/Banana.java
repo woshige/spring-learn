909990c3f7a39669ccfcc911b7529c52a43715ac
unknown <sunlichao@ESG.360ES.CN>
2020-01-10 11:09:55 +0800

    'add'

--- sprint-bean-profile/src/main/java/com/spring/bean/profile/food/Banana.java
+++ sprint-bean-profile/src/main/java/com/spring/bean/profile/food/Banana.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * 当另一个类中使用属性注入一个接口的时候，如果说一个接口的实现类有多个的话，会让spring容器抛出异常
- * 所以对待这种情况的时候，使用@Primary注解来指定那个对象是要被优先的进行创建的
+ * 所以对待这种情况的时候，使用@Primary注解来指定那个对象是要被优先的进行创建的，就是首选的意思
+ * 但是有种情况是首选的对象不止一个所以可以使用注解@Qualifier
  */
 @Component
 @Primary
--- sprint-bean-profile/src/main/java/com/spring/bean/profile/food/Bread.java
+++ sprint-bean-profile/src/main/java/com/spring/bean/profile/food/Bread.java
@@ -1,8 +1,10 @@
 package com.spring.bean.profile.food;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Bread implements Food {
     @Override
     public void eat() {
